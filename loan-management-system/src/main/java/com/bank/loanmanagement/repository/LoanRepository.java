4d0baa2e820da6e1fcea05442e1edaba9fc8688f
vijayhanumanthu <vijayhanumanthums@gmail.com>
2026-06-24 16:18:57 -0500

    LoanRepo added

---
+++ loan-management-system/src/main/java/com/bank/loanmanagement/repository/LoanRepository.java
@@ -0,0 +1,17 @@
+package com.bank.loanmanagement.repository;
+
+import com.bank.loanmanagement.model.Loan;
+import com.bank.loanmanagement.model.LoanStatus;
+import com.bank.loanmanagement.model.LoanType;
+import com.bank.loanmanagement.model.RiskLevel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+    
+}
