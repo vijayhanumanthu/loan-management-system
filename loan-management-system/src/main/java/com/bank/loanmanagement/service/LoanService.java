7f74742c0cd9c9c9da9266f0313efc40204dd9d4
vijayhanumanthu <vijayhanumanthums@gmail.com>
2026-07-08 14:18:31 -0500

    service class added

---
+++ loan-management-system/src/main/java/com/bank/loanmanagement/service/LoanService.java
@@ -0,0 +1,14 @@
+package com.bank.loanmanagement.service;
+
+import com.bank.loanmanagement.model.*;
+import com.bank.loanmanagement.repository.LoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LoanService {
+    
+}
